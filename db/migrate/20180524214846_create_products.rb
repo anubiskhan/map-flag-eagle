aa36340ac993c675e7f0334a13f4574f4c7f6010
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-24 15:51:21 -0600

    Adds product migration and migrates

---
+++ db/migrate/20180524214846_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.numeric :price, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
