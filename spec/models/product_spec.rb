b4c805c80f30deddc8e675026321a3786d1f08e9
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-24 15:56:57 -0600

    Improves product model test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Product do
   describe 'attributes' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:price) }
   end
