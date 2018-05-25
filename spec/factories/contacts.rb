86c1514f12ebdc5cd9da2ba13280cd3d642fc486
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-24 18:03:56 -0600

    Fixes typo in product spec, deletes contact factory due to error, adjusts failed faker calls in products factory

--- spec/factories/contacts.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :contact do
-    address "#{Faker::Address.street_address} #{Faker::Address.city}, #{Faker::Address.state} #{Faker::Address.zip} "
-    phone Faker::PhoneNumber.phone_number
-    user
-  end
-end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :product do
-    name Faker::Commerce.product_name
-    description Faker::Myst.quote
+    name 'A Name'
+    description Faker::Fallout.quote
     image "http://i.imgur.com/BCbxVui.png"
-    price Faker::Commerce.product_price
+    price 3.50
   end
 end
