2a10740ede96b884c5bd976cfb339dfea4147be7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-28 14:00:39 -0600

    Adds logout path

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2', as: :google_oauth2_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get :logout, to: 'sessions#destroy'
 end
