c3c1cddea38bd3c65f8671cdfe04044574622a28
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-02 12:06:29 -0600

    Adds namespaced routes for map contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/maps/new', to: 'maps#new'
+      post '/maps', to: 'maps#create'
+    end
+  end
+
   get '/', to: 'welcome#index'
 
   get '/auth/google_oauth2', as: :google_oauth2_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get :logout, to: 'sessions#destroy'
   resources :orders, only: [:new]
-  resources :maps, only: [:new, :create]
+  resources :maps, only: [:new]
 end
