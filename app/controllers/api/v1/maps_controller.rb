6d868517a1a1d1dc55e57555ff499b2bef5d116a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-02 12:38:50 -0600

    Removes binding pry

--- app/controllers/api/v1/maps_controller.rb
+++ app/controllers/api/v1/maps_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MapsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    binding.pry
     @map = Map.new(map_params[:lat], map_params[:long], map_params[:zoom])
     # Happy/sad path logic, build out table for map
     redirect_to new_order_path
