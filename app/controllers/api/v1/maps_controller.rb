fa7d5b0e4194b23364a62b5b5ed2222925e0723e
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-02 12:30:32 -0600

    Adds maps api controller for create

---
+++ app/controllers/api/v1/maps_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MapsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    binding.pry
+    @map = Map.new(map_params[:lat], map_params[:long], map_params[:zoom])
+    # Happy/sad path logic, build out table for map
+    redirect_to new_order_path
+  end
+
+  private
+
+  def map_params
+    params.permit(:lat, :long, :zoom)
+  end
+end
