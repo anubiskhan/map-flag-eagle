d3969e4c7f9a2300691d800bf9dcc7bb48d7aa89
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-02 12:38:08 -0600

    Removes require map from strong params, moves create method to api controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,18 +2,9 @@ class MapsController < ApplicationController
   def new
 
   end
-
-  def create
-    @map = Map.new(map_params[:lat], map_params[:long], map_params[:zoom])
-    binding.pry
-    { "success": "Utilize this map" }
-  end
-
   private
 
-
   def map_params
-    binding.pry
-    params.require(:map).permit(:lat, :long, :zoom)
+    params.permit(:lat, :long, :zoom)
   end
 end
