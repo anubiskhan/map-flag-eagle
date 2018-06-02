787ead4862d403bdbdb2d4b8123de866a0e4395d
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-02 12:18:19 -0600

    Fixes map routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/maps/new', to: 'maps#new'
       post '/maps', to: 'maps#create'
     end
   end
@@ -13,5 +12,5 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get :logout, to: 'sessions#destroy'
   resources :orders, only: [:new]
-  resources :maps, only: [:new]
+  get '/maps/new', to: 'maps#new'
 end
