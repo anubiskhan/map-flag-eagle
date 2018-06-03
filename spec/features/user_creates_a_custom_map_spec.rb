06d844357a7bfdbabfd1e2833619b3bf34f1a895
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-03 08:46:34 -0600

    Adjusts DB cleaner settings

--- spec/features/user_creates_a_custom_map_spec.rb
+++ spec/features/user_creates_a_custom_map_spec.rb
@@ -1,14 +1,31 @@
 require 'rails_helper'
 
 describe 'User', js: true do
-  it 'creates a custom map' do
-    visit maps_new_path
+  describe 'creates a custom map' do
+    it 'adds a map to the DB' do
+      visit maps_new_path
 
-    expect(page).to have_css('#map')
+      expect(page).to have_css('#map')
+      map_count = Map.all.count
+      click_on 'Use This Map'
 
-    click_on 'Use This Map'
+      expect(Map.all.count).to_not eq(map_count)
+      expect(current_path).to eq(new_order_path)
+    end
+    it 'targets the map properly' do
+      visit maps_new_path
 
-    expect(Map.all.count).to eq(1)
-    expect(current_path).to eq(new_order_path)
+      fill_in 'address', with: 'spokane'
+
+      click_on 'Encode'
+
+      click_on 'Use This Map'
+
+      map = Map.last
+
+      expected_url = "http://maps.googleapis.com/maps/api/staticmap?&center=39.7392,-104.99029999999999&zoom=14&size=750x450&key=AIzaSyCJwmkCQw56cvtU03ftjoN1SpzL1VmTov4&style=feature:administrative|element:all|saturation:-100&style=feature:administrative.province|element:all|visibility:off&style=feature:landscape|element:all|saturation:-100|lightness:65|visibility:on&style=feature:poi|element:all|visibility:off&style=feature:road|element:all|saturation:-100&style=feature:road.highway|element:all|visibility:simplified&style=feature:road.arterial|element:all|lightness:30&style=feature:road.local|element:all|lightness:40&style=feature:transit|element:all|saturation:-100|visibility:simplified&style=feature:water|element:geometry|hue:0xffff00|lightness:-25|saturation:-97&style=feature:water|element:labels|lightness:-25|saturation:-100"
+
+      expect(map['static_url']).to eq(expected_url)
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'database_cleaner'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -45,20 +47,13 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
