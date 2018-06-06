6dc514fb5d1ca8c143bd086184fdafa572a9f07b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 23:47:32 -0600

    Adds button test for landing page

--- spec/features/user_visits_landing_page.rb
+++ spec/features/user_visits_landing_page.rb
@@ -8,5 +8,12 @@ describe 'User' do
       expect(page).to have_content('Custom Laser Cut Maps!')
       expect(page).to have_css('#myCarousel')
     end
+    it 'clicks on make it your own' do
+      visit '/'
+
+      click_on 'Make it your own!'
+
+      expect(current_path).to eq("#{maps_new_path}")
+    end
   end
 end
