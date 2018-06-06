f0d98e7d814a039bd83d9a894b2a04d69c6e94cc
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 23:06:50 -0600

    Adds relationships to several models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   validates_presence_of :name, :description, :price
   validates_uniqueness_of :name
+
+  has_many :product_orders
+  has_many :orders, through: :product_orders
 end
