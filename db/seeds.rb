632b37d64088146f5303631307f9b18d1204f3b9
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 23:05:57 -0600

    Adds column to products to reference a specific map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(
+  name: 'Custom Map',
+  description: 'Pick the location and zoom of your own map!',
+  price: 39,
+)
