fa7055a1f3ee70f72d8dc63f012e1714518503a2
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 21:37:46 -0600

    Adds cart spec and cart

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Cart do
+  context 'methods' do
+    context '#add_product' do
+      it 'adds a product to product_list' do
+        cart = Cart.new({'8' => 2, '6' => 4})
+        cart.add_product(8)
+        cart.add_product(6)
+
+        expect(cart.product_list).to eq({'8' => 3, '6' => 5})
+      end
+    end
+    context '#total_product_count' do
+      it 'returns the total number of products' do
+        cart = Cart.new({'8' => 2, '6' => 4})
+
+        expect(cart.total_product_count).to eq(2)
+      end
+    end
+    context '#total_price' do
+      it 'makes an active record collection of product objects' do
+        product_1 = create(:product)
+        product_2 = create(:product)
+        cart = Cart.new(nil)
+        cart.add_product(product_1.id)
+        cart.add_product(product_1.id)
+        cart.add_product(product_2.id)
+
+        expect(cart.total_price).to eq((product_1.price * 2) + product_2.price)
+      end
+    end
+  end
+end
